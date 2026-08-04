55c24209cb34af5bf3b60fc8ac8e274405c92ceb
UditNarayanaKB <uditnarayanakb@gmail.com>
2026-08-04 09:51:42 +0530

    ER diagrmas, Postgres

---
+++ src/11 - Actuataors/postgress/src/test/java/org/bank/postgress/PostgressApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bank.postgress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostgressApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
